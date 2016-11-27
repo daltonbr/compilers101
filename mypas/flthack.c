8672d5cf3dae847e7d761fddc37b4531bdb505b2
daltonbr <sacinopatinete@gmail.com>
2016-11-27 18:48:41 -0200

    add a disassembly  code

--- mypas/flthack.c
+++ mypas/flthack.c
@@ -8,6 +8,18 @@ double xx = 3.14159;
 
 int main()
 {
-	i = i + 3;
+	i = i * 60;
+    x = x * 60;
+    xx =-xx; 
+    
+    //printf("i = %i, x=%f, xx=%lf\n",i,x,xx);
 
+    return 0;
 }
+
+/*
+60
+32u = 2ˆ5 << 5
+64u = eax+eax 
+-4u (4xsub)
+*/
--- mypas/flthack.s
+++ mypas/flthack.s
@@ -1,43 +1,55 @@
-	.file	"flthack.c"
-	.globl	i
-	.data
-	.align 4
-	.type	i, @object
-	.size	i, 4
-i:
-	.long	3
-	.globl	x
-	.align 4
-	.type	x, @object
-	.size	x, 4
-x:
-	.long	1078530000
-	.globl	xx
-	.align 8
-	.type	xx, @object
-	.size	xx, 8
-xx:
-	.long	4028335726
-	.long	1074340345
-	.text
-	.globl	main
-	.type	main, @function
-main:
-.LFB0:
+	.section	__TEXT,__text,regular,pure_instructions
+	.macosx_version_min 10, 12
+	.section	__TEXT,__literal4,4byte_literals
+	.align	2
+LCPI0_0:
+	.long	1114636288              ## float 60
+	.section	__TEXT,__text,regular,pure_instructions
+	.globl	_main
+	.align	4, 0x90
+_main:                                  ## @main
 	.cfi_startproc
+## BB#0:
 	pushq	%rbp
+Ltmp0:
 	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
+Ltmp1:
+	.cfi_offset %rbp, -16
 	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
-	movl	i(%rip), %eax
-	addl	$3, %eax
-	movl	%eax, i(%rip)
+Ltmp2:
+	.cfi_def_cfa_register %rbp
+	xorl	%eax, %eax
+	movss	LCPI0_0(%rip), %xmm0    ## xmm0 = mem[0],zero,zero,zero
+	movl	$0, -4(%rbp)
+	imull	$60, _i(%rip), %ecx
+	movl	%ecx, _i(%rip)
+	mulss	_x(%rip), %xmm0
+	movss	%xmm0, _x(%rip)
+	movsd	_xx(%rip), %xmm0        ## xmm0 = mem[0],zero
+	movd	%xmm0, %rdx
+	movabsq	$-9223372036854775808, %rsi ## imm = 0x8000000000000000
+	xorq	%rsi, %rdx
+	movd	%rdx, %xmm0
+	movsd	%xmm0, _xx(%rip)
 	popq	%rbp
-	.cfi_def_cfa 7, 8
-	ret
+	retq
 	.cfi_endproc
-.LFE0:
-	.size	main, .-main
-	.ident	"GCC: (GNU) 4.8.3 20140911 (Red Hat 4.8.3-7)"
-	.section	.note.GNU-stack,"",@progbits
+
+	.section	__DATA,__data
+	.globl	_i                      ## @i
+	.align	2
+_i:
+	.long	3                       ## 0x3
+
+	.globl	_x                      ## @x
+	.align	2
+_x:
+	.long	1078530000              ## float 3.14159012
+
+	.globl	_xx                     ## @xx
+	.align	3
+_xx:
+	.quad	4614256650576692846     ## double 3.1415899999999999
+
+
+.subsections_via_symbols
