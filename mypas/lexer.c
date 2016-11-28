8d2686460390eed791e2abb63b309cdf86456679
daltonbr <sacinopatinete@gmail.com>
2016-11-27 22:18:17 -0200

    add isdecimal() [lexer.c]

--- mypas/lexer.c
+++ mypas/lexer.c
@@ -12,7 +12,6 @@ char lexeme[MAXID_SIZE+1];
 
 void skipspaces (FILE *tape)
 {
-    
 	lexeme[0] = getc(tape);
     while ( lexeme[0] != '\n' && isspace (lexeme[0]) ) {
         lexeme[0] = getc(tape);
@@ -21,7 +20,6 @@ void skipspaces (FILE *tape)
         ungetc ( lexeme[0], tape );
 }
 
-//hoje
 int isassign (FILE * tape)
 {
 	if ((lexeme[0] = getc(tape)) == ':') {
@@ -34,7 +32,25 @@ int isassign (FILE * tape)
 	ungetc(lexeme[0], tape);
 	return 0;
 }
-//-hoje
+
+int isdecimal (FILE *tape) {
+	int i = 0;
+
+	if (isdigit (lexeme[i] = getc(tape) ) ) {
+		if (lexeme[i] == '0') {
+			++i;
+			lexeme[i] = getc(tape);
+			return i;
+		}
+		// [0-9]*
+		for(++i; isdigit( lexeme[i] = getc(tape) ); ++i) {
+			if(i >= MAXID_SIZE) i = MAXID_SIZE;
+		}
+		return i;
+	}
+	return 0;
+}
+
 
 int isidentifier(FILE *tape)
 {
@@ -50,22 +66,6 @@ int isidentifier(FILE *tape)
         ungetc (lexeme[0], tape);
         return 0;
 }
-//hoje
-int isint(FILE *tape)
-{
-        if (isdigit (lexeme[0] = getc(tape)) ) {
-                if (lexeme[0] == '0') {
-                        return INT;
-                }
-                // [0-9]*
-                while ( isdigit (lexeme[0] = getc(tape)) );
-                ungetc (lexeme[0], tape);
-                return INT;
-        }
-        ungetc (lexeme[0], tape);
-        return 0;
-}
-//-hoje
 
 /*int isoctal(FILE *tape)
 {
@@ -89,20 +89,21 @@ int isint(FILE *tape)
 int gettoken (FILE *sourcecode)
 {
         int token;
-
         skipspaces (sourcecode);
 
-        if ( token = isidentifier(sourcecode) ) {
-                return ID;
-        }
+        if( (token = isidentifier(sourcecode)) ) {
+		    return token;
+	    }
 
-        if ( token = isint (sourcecode) ) {
-                return INT;
-        }
+	    if( (token = isassign(sourcecode)) ) {
+		    return token;
+	    }
+
+	    if( (token = isfloat (sourcecode)) ) {
+		    return token;
+	    }
 	
-	lexeme[1] = 0;
-
+	    lexeme[1] = 0;
         lexeme[0] = token = getc (sourcecode);
-
         return token;
 }
