319394e45cd405617943da421b3a13234c777b8d
Guilherme Lopes Ferreira <gferreira525@gmail.com>
2016-12-01 14:50:08 +0000

    adicionando numeros das keywords

--- mypas/keywords.c
+++ mypas/keywords.c
@@ -4,31 +4,31 @@
 
 
 char *keywords[] = {
-        "begin",
-        "if",
-        "then",
-        "elif",
-        "else",
-        "endif",
-        "while",
-        "do",
-        "repeat",
-        "until",
-        "procedure",
-        "function",
-        "var",
-        "boolean",
-        "integer",
-        "real",
-        "double",
-        "div",
-        "mod",
-        "and",
-        "or",
-        "not",
-        "true",
-        "false",
-        "end"
+        "begin",//524288
+        "if",//524289
+        "then",//524290
+        "elif",//524291
+        "else",//524292
+        "endif",//524293
+        "while",//524294
+        "do",//524295
+        "repeat",//524296
+        "until",//524297
+        "procedure",//524298
+        "function",//524299
+        "var",//524300
+        "boolean",//524301
+        "integer",//524302
+        "real",//524303
+        "double",//524304
+        "div",//524305
+        "mod",//524306
+        "and",//524307
+        "or",//524308
+        "not",//524309
+        "true",//524310
+        "false",//524311
+        "end"//524312
 };
 
 int iskeyword(char const *identifier)
