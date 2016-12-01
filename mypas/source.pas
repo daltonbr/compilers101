7f8c28d0642ea44ff07d45a4b327d5eed746969e
daltonbr <sacinopatinete@gmail.com>
2016-12-01 14:42:39 -0200

    update source.pas [int, float and double declared]

--- mypas/source.pas
+++ mypas/source.pas
@@ -1,14 +1,20 @@
 var x, y : integer;
+    z : float;
+    w : double;
 begin
     x := 1;
     y := 2;
+    z := 3.14;
+    w := 3.14159;
 
     if x > 3 then
         x := 2 * y + 4;
+        w := w + 1.2;
     elif x > 5 then
         x := 10;
     else
         x := 20;
+        z := z + 1.1;
     y := x * y;
     endif;
     while x < 2 do
