eaa457172cb389eee2adad45866ee266690cd4f6
Maicon Berbert <kakobrt@gmail.com>
2016-12-01 14:14:40 -0200

    fix comments

--- mypas/keywords.h
+++ mypas/keywords.h
@@ -13,21 +13,21 @@ enum{
     UNTIL, //524297
     PROCEDURE, //524298
     FUNCTION, //524299
-    WRITE,
-    VAR, //524300
-    BOOLEAN, //524301
-    INTEGER, //524302
-    REAL, //524303
-    DOUBLE, //524304
-    DIV, //524305
-    MOD, //524306
-    AND, //524307
-    OR, //524308
-    NOT, //524309
-    TRUE, //524310
-    FALSE, //524311
-	PROGRAM, //524312
-    END //524313
+    WRITE, //524300
+    VAR, //524301
+    BOOLEAN, //524302
+    INTEGER, //524303
+    REAL, //524304
+    DOUBLE, //524305
+    DIV, //524306
+    MOD, //524307
+    AND, //524308
+    OR, //524309
+    NOT, //524310
+    TRUE, //524311
+    FALSE, //524312
+	PROGRAM, //524313
+    END //524314
 };
 
 extern char *keywords[];
